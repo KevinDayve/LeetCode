1ebf548b3353c18d21248272f3c8f8b48dd1e8f7
KevinDayve <82874516+KevinDayve@users.noreply.github.com>
2023-01-31 14:32:07 +0000

    Update BinarySearch.py
    Created an order agnostic binary search algorithm.

--- BinarySearch.py
+++ BinarySearch.py
@@ -3,14 +3,24 @@ Class Binary:
     start = 0
     end = len(array) - 1
     
-    while start <= end:
+    #find whether the array is ascending or descending
+    isasc = bool(array[start] < array[end])
+      
+    
+    while start <= end and isasc:
       m = start + (end - start) // 2
-      if value < array[m]:
-        end = m - 1
-      elif value > array[m]:
-        start = m + 1
-      else:
+      if arr[m] == target:
         return m
+      if isasc:
+        if value < array[m]:
+          end = m - 1
+        elif value > array[m]:
+          start = m + 1
+      else:
+        if value > array[m]:
+          end = m - 1
+        elif value < array[m]:
+          start = m + 1
       
     return "Value doesn't exist."
   
