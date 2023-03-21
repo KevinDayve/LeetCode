7ffaae44560d94d7964000911574e4d5d82d30eb
Kevin Dave <82874516+KevinDayve@users.noreply.github.com>
2023-03-21 05:22:13 +0000

    Update nto1.py

--- nto1.py
+++ nto1.py
@@ -7,6 +7,14 @@ def nto1(n):
     print(n)
     return nto1(n-1)
   
+ def one_to_n(n):
+  if n == 0:
+    return
+  else:
+    one_to_n(n-1)
+    print(n)
   
   
-  print(nto1(10))
+  
+print(nto1(10))
+print(one_to_n(10))
